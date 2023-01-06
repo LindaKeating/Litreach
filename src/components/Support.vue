a3922d87bb05deed2000a59bc5cfe5b215262830
Linda Keating <lindakeating@Lindas-MacBook-Pro.local>
2023-01-06 22:28:40 +0000

    change debugging script

--- src/components/Support.vue
+++ src/components/Support.vue
@@ -3,7 +3,7 @@
     <p className="SettingModal-paragraph">             
       Má thugann tú faoi deara rud ar bith faoin chluiche seo nach bhfuil ag obair i gceart, bheadh
 muid buíoch dá gcuirfeá ar an eolas muid trí
-        <a :href="`mailto:linda.mary.keating@gmail.com?subject=Fadhb le Litreach&body=**** Seo iad mo shonraí width:${screenWidth} height:${screenHeight} userAgent:${userAgent} platform:${platform} vendor:${vendor} version: ${version}`"> ríomhphost a sheoladh </a> chugainn. 
+        <a :href="`mailto:linda.mary.keating@gmail.com?subject=Fadhb le Litreach&body=**** Seo iad mo shonraí Leithead:${screenWidth} Airde:${screenHeight} userAgent:${userAgent} Ardán:${platform} díoltóir:${vendor} leagan: ${version}`"> ríomhphost a sheoladh </a> chugainn. 
                Mínigh an fhadhb, chomh cruinn agus is féidir, agus déanfaidh muid ár ndícheall é a chóiriú chomh luath agus is féidir linn.
     </p>
     <p>{{ dictionary.SupportBrowser.ga }}</p>
